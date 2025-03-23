a07683fccd71e094d1bad0cb654aa38e7884e693
gaabrielmooreira <gaabrielmooreira@gmail.com>
2025-03-23 18:38:34 -0300

    feat: add pattern Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+
+namespace Singleton
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Singleton singleton1 = Singleton.GetInstance();
+            Singleton singleton2 = Singleton.GetInstance();
+
+            Console.WriteLine($"Is same instance? {singleton1 == singleton2}");
+        }
+    }
+
+    public sealed class Singleton
+    {
+        private static Singleton? _instance;
+        private static readonly object _lock = new object();
+
+        private Singleton() { }
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+
+}
+
