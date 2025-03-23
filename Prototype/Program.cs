4d0756229db3288fd0dc73345f1df36177c4217b
gaabrielmooreira <gaabrielmooreira@gmail.com>
2025-03-22 23:55:17 -0300

    feat: add pattern Prototype

---
+++ Prototype/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json;
+
+namespace Prototype
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var car = new Car()
+            {
+                Wheels = "G",
+                Engine = new Engine()
+                {
+                    Name = "Exemplo",
+                    SerialNumber = 400
+                }
+            };
+            Console.WriteLine(JsonSerializer.Serialize(car));
+            Console.WriteLine(":::::::::: CLONE :::::::::");
+            var carClone = car.Clone();
+            Console.WriteLine(JsonSerializer.Serialize(carClone));
+        }
+    }
+}
+
+public class Car : ICloneable
+{
+    public string Wheels { get; set; }
+    public Engine Engine { get; set; }
+    public object Clone()
+    {
+        return new Car()
+        {
+            Wheels = this.Wheels,
+            Engine = new Engine()
+            {
+                Name = this.Engine.Name,
+                SerialNumber = this.Engine.SerialNumber
+            }
+        };
+    }
+}
+
+public class Engine
+{
+    public string Name { get; set; }
+    public int SerialNumber { get; set; }
+}
